079bd2ef49403953d0c29639a1d59c9bf902a168
Brad Traversy <traversymedia@gmail.com>
2019-09-04 08:48:47 -0400

    polls details and results

--- pollster/polls/urls.py
+++ pollster/polls/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results')
 ]
--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -7,3 +7,16 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+# Show specific question and choices
+def detail(request, question_id):
+  try:
+    question = Question.objects.get(pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404("Question does not exist")
+  return render(request, 'polls/detail.html', { 'question': question })
+
+# Get question and display results
+def results(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', { 'question': question })
