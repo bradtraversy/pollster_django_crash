67bac64052309e8607d6e71bcbc4d617045dc3de
Brad Traversy <traversymedia@gmail.com>
2019-09-04 08:53:41 -0400

    pages app/landing page

--- pollster/pollster/urls.py
+++ pollster/pollster/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('pages.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
